79d2ee9ca96e63a92c4e250bcb7a40951308f7b3
neohlm <neohlumbene@gmail.com>
2025-07-23 20:55:08 +0200

    background selection

--- script.js
+++ script.js
@@ -9,3 +9,19 @@ function toggleMenu(){
     icon.classList.toggle("open");
 
 }
+
+document.querySelector(".section_text_p2").textContent = "";
+
+//typewriter effect
+let i = 0;
+const txt = 'Final Year Student - Aspiring Software Engineer - Cyber Security Enthusiast';
+const speed = 50;
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.querySelector(".section_text_p2").textContent += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+window.onload = typeWriter;
